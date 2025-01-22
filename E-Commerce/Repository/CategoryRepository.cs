4f3bf653df2a74cab77088e3b627360aca4dbbb3
AHMED ATEF <ahmedatefabdelazeem@gmail.com>
2025-01-22 09:39:58 +0200

    Create Category Repository And Implement Add,GetById Function

---
+++ E-Commerce/Repository/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private AppDbContext context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Category> GetByIdAsync(string Id)
+        {
+            return await context.Categories.FirstOrDefaultAsync(x => x.CategoryId == Id);
+        }
+        public async Task AddAsync(Category model)
+        {
+            model.CategoryId =  Guid.NewGuid().ToString();
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Category>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
