1497ace40042883412825d5a9fe755c56f24910b
AHMED ATEF <ahmedatefabdelazeem@gmail.com>
2025-01-22 08:46:40 +0200

    Create Generic Interface That Contains Common Crud Operation

---
+++ E-Commerce/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Repository
+{
+    public interface IRepository <Type>
+    {
+        Task AddAsync(Type model);
+        Task UpdateAsync(Type model);
+        Task DeleteAsync(string Id);
+        Task<Type> GetByIdAsync(string Id);
+        Task<List<Type>> GetAllAsync();
+    }
+}
